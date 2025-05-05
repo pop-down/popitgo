4c9e5c8c9ff9e918131e222583a8f6067e1136fb
pop-down <low.essenc@gmail.com>
2025-05-06 04:22:52 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
